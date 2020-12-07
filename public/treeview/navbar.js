2e7c5bf819f93dcff3174e15ee9056fffab8537f
zaxer2 <william.johns3@gmail.com>
2020-12-06 16:31:46 -1000

    FINISH navbar.js besides style changes

--- public/treeview/navbar.js
+++ public/treeview/navbar.js
@@ -1,11 +1,51 @@
+/**
+ * "public" array currentlyActiveSpecies- contains all currently active species as strings. Ex: ["Polydesmus", "Somethus", "Glomeris"]
+ */
+var currentlyActiveSpecies = [];
+
+/**
+ * "public" Bool to clamp the size of dataset. Should be false almost always.
+ */
+var testingMode = false;
+
+/**
+ * "public" key-value pair map of scores. two-letter String-> int. Example. "AA" -> 4
+ */
+var scoreMap = new Map();
+
+/**
+ * Outside functions should be put in here. pingChange is called every time a change occurs in the currentlyActiveSpecies[]
+ */
+function pingChange() 
+{
+	console.log("active species updated!");
+	console.log(currentlyActiveSpecies);
+	//todo: put outside references here
+}
+
+/**
+ * Simple getter for currentlyActiveSpecies[]
+ */
+function getCurrentSpecies() 
+{
+	return currentlyActiveSpecies;
+}
+
+
+
+
+
+/**
+ * 
+ * 
+ * BEGIN OBSCURE NONSENSE. BEWARE YE ALL WHO VENTURE PAST THIS POINT
+ * 
+ * 
+ */
+
+ 
+//function called on page load
 document.body.onload = loadTree;
-
-var currentlyActiveSpecies = []; //Reference this or getCurrentSpecies for a list of all checked species
-
-var testingMode = false; //HARD-CAPS NUM LINES READ TO 400
-
-var scoreMap = new Map(); //key-value pair map of scores. String-> int. Example. "AA" -> 4
-
 function loadTree () { 
 
 	jQuery.get('treeview/matrix.txt', function(matrix) {
@@ -16,21 +56,19 @@ function loadTree () {
 
 }
 
-function getCurrentSpecies() {
-	return currentlyActiveSpecies;
-}
-
 function addCheckBox(name, parent = document.getElementById("tree")) {
 	if(document.getElementsByClassName(name.split(" ").join("")).length > 0) {return document.getElementsByClassName(name.split(" ").join(""))[0];}
   // create a new div element 
   const newDiv = document.createElement("div"); 
   
   // and give it some content 
+  newDiv.style.marginLeft = "25px";
   const newContent = document.createTextNode("  " + name); 
   const newCheckbox = document.createElement("INPUT");
   newCheckbox.setAttribute("type", "checkbox");
   newCheckbox.setAttribute("value", name);
-  newCheckbox.onclick = function() {checkboxClick(newCheckbox.value, newCheckbox.checked)}
+  newCheckbox.classList.add("checkbox");
+  newCheckbox.onclick = function() {checkboxClick(newCheckbox.value, newCheckbox.checked); checkAbove(newDiv);}
   
   // add the text node to the newly created div
   newDiv.classList.add(name.split(" ").join(""));
@@ -48,21 +86,83 @@ function addCheckBox(name, parent = document.getElementById("tree")) {
   parent.appendChild(newDiv);
 }
 
+function checkAbove(element) {
+	console.log("checking elements above!");
+	var parent = element.parentElement;
+	if (parent == document.getElementById("tree")) {
+		return;
+	}
+	var checkbox = parent.querySelector(".checkbox");
+
+	var allAreEnabled = true;
+	var allAreDisabled = true;
+
+	var childCheckboxes = parent.querySelectorAll(".checkbox");
+	console.log(childCheckboxes.length);
+
+	childCheckboxes.forEach(childbox => {
+		if (!allAreDisabled && !allAreEnabled) {
+			return;
+		}
+		if (childbox != checkbox) {
+			if(childbox.checked) {
+				allAreDisabled = false;
+			} else {
+				allAreEnabled = false;
+			}
+		}
+	})
+
+	if (allAreEnabled) {
+		checkbox.checked = true;
+		checkbox.indeterminate = false;
+		console.log("all are enabled!");
+	} 
+	else if (allAreDisabled) {
+		checkbox.checked = false;
+		checkbox.indeterminate = false;
+		console.log("disabled!");
+	} else {
+		checkbox.checked = true;
+		checkbox.indeterminate = true;
+		console.log("indeterm!");
+	}
+
+	checkAbove(parent);
+
+
+}
+function propogateBelow(element, checked) {
+	console.log("propogating elements below with " + checked);
+	var childCheckboxes = element.querySelectorAll(".checkbox");
+
+	var changedBoxes = [];
+	childCheckboxes.forEach(childbox => {
+		childbox.checked = checked;
+		childbox.indeterminate = false;
+		if (childbox.value != "on") {
+			checkboxClick(childbox.value, checked, false)
+			
+		}
+	})
+
+}
 //Calls every time a checkbox value toggles
-function checkboxClick(value, nowChecked) {
+function checkboxClick(value, nowChecked, pingChange = true) {
 	if (nowChecked) {
 		console.log(value + " added to active!");
 		if (!currentlyActiveSpecies.includes(value)) {
 			currentlyActiveSpecies.push(value);
 		}
-		console.log(currentlyActiveSpecies);
 	} else {
 		console.log(value + " removed from active!");
 		if (currentlyActiveSpecies.includes(value)) {
 			var index = currentlyActiveSpecies.indexOf(value);
 			currentlyActiveSpecies.splice(index, 1);
 		}
-		console.log(currentlyActiveSpecies);
+	}
+	if (pingChange) {
+		pingChange();
 	}
 
 	//
@@ -73,10 +173,16 @@ function createNewParentDiv(name, parent = document.getElementById("tree")) {
 	if(document.getElementsByClassName(name.split(" ").join("")).length > 0 || (name.length < 2)) { return document.getElementsByClassName(name.split(" ").join(""))[0];}
     const newDiv = document.createElement("div"); 
 	const newContent = document.createElement("div");
-	newContent.appendChild(document.createTextNode(name));
+	const newCheckbox = document.createElement("INPUT");
+	newCheckbox.setAttribute("type", "checkbox");
+	newCheckbox.classList.add("checkbox");
+	newContent.appendChild(newCheckbox);
+	newContent.appendChild(document.createTextNode("  " + name));
 	newContent.style.cursor = "pointer";
 	newContent.style.userSelect = "none";
 	newDiv.style.backgroundColor = "lightblue";
+
+
 
 	newContent.addEventListener("click", function() {
 		var children = this.parentElement.querySelectorAll(".nested");
@@ -90,6 +196,12 @@ function createNewParentDiv(name, parent = document.getElementById("tree")) {
 		
 		console.log("clicked one!");
 	});
+	newCheckbox.addEventListener("click", function(event) {
+		event.stopPropagation();
+		propogateBelow(newDiv, newCheckbox.checked);
+		checkAbove(newDiv);
+		pingChange();
+	})
 
 	newDiv.appendChild(newContent);
 	newDiv.style.paddingLeft = "20px";
@@ -166,16 +278,6 @@ function do_something_with(data, matrix) {
 			parent = parent.replace(/(\r\n|\n|\r)/gm, ""); //removing bs invisible characters
                 if (parent === half2[0]) { //if the name of the next parent is the name of the species, end treebuilding.
 					pleaseBreak = true;
-					if (parent == "" && half2[0] == "") {
-						console.log("uh oh");
-					}
-				} else {
-					if (parent.includes(half2[0])) {
-						console.log("parent \"" + JSON.stringify(parent) + "\" includes \"" + JSON.stringify(half2[0]) + "\" error")
-					}
-					if (half2[0].includes(parent)) {
-						console.log("checkbox \"" + JSON.stringify(half2[0]) + "\" includes \"" + JSON.stringify(parent) + "\" error")
-					}
 				}
 				if (!pleaseBreak) {
 					currentParent = createNewParentDiv(parent, currentParent); //create current parent, and update tracked current parent
